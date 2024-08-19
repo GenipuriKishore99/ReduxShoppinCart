de6e6dd7fb915095e875e432b2986ec6a3c262e9
GenipuriKishore <GenipuriKishore99@gmail.com>
2024-08-19 16:28:52 +0530

    change data

--- src/components/Chnages.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Chnages = () => {
-  return (
-    <div>Chnages</div>
-  )
-}
-
-export default Chnages
---
+++ src/components/Kishore1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Kishore1 = () => {
+  return (
+    <div>Kishore1</div>
+  )
+}
+
+export default Kishore1
